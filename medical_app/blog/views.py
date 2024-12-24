e256bc38d397ceeefe88bb9c7bd54f78b577d7d2
claymef <claymef@unsa.edu.pe>
2024-12-24 01:17:32 -0500

    Initial commit

---
+++ medical_app/blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
+
+class PostListAPIView(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
